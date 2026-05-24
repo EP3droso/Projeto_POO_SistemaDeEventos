62d8b33557656e9da3b557ca4ce58d2397f5239b
Eduardo <eduardo.hpedroso123@gmail.com>
2026-05-24 18:58:56 -0300

    Ajustes do loop principal e criação da classe Utilitarios

--- SistemaDeEventos/src/ucs/poo/trabalho_eventos/main/Main.java
+++ SistemaDeEventos/src/ucs/poo/trabalho_eventos/main/Main.java
@@ -2,33 +2,127 @@ package ucs.poo.trabalho_eventos.main;
 
 import java.util.Scanner;
 
+import ucs.poo.trabalho_eventos.models.*;
+import ucs.poo.trabalho_eventos.main.Utilitarios;
+
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String stringEntrada;
-		int intEntrada;
-		
-		System.out.println("Seja bem vindo ao menu principal do Sistema de Controle de Eventos, o menu visual será inserido posteriormente");
-		
-		while(!"0".equals(stringEntrada = sc.nextLine())) {
-			try {
-				intEntrada = Integer.parseInt(stringEntrada);
-				switch(intEntrada) {
-				case 1:
-					System.out.println("Em andamento");
-				}
-			}
-			catch(NumberFormatException e){
-				System.out.println("Entrada não corresponde a nenhuma das opções do menu");
-			}
-		}
+	
+	private String nomeEmpresa;
+	
+	private String getNomeEmpresa() {
+		return nomeEmpresa;
+	}
+
+	private void setNomeEmpresa(String nomeEmpresa) {
+		this.nomeEmpresa = nomeEmpresa;
 	}
 	
-	private void mostrarMenu() {
-		System.out.println("MENU");
+	private static void mostrarMenuPrincipal(Main main) {
+		System.out.println("MENU de " + main.getNomeEmpresa());
 		System.out.println("1 - Cadastro de Evento");
 		System.out.println("2 - Menu de Eventos");
 		System.out.println("3 - Menu de Recursos");
 		System.out.println("4 - Relatorios");
+		System.out.println("0 - Sair");
+	}
+	
+	
+	private static void mostrarMenuEventos(){
+		System.out.println("MENU de Eventos");
+		System.out.println("1 - Cadastro de Tarefa");
+		System.out.println("0 - Sair");
+	}
+	
+	
+	private static void login(Main main) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("LOGIN");
+		System.out.println("Insira o nome de sua Empresa:");
+		String nomeEmpresa = sc.nextLine();
+		main.setNomeEmpresa(nomeEmpresa);
+		System.out.println("Insira seu usuario:");
+		String nome = sc.nextLine();
+		System.out.println("Insira sua senha:");
+		sc.next();
+		System.out.println("Seja muito bem vindo " + nome + "!");
+		
+	}
+	
+
+
+	public static void main(String[] args) {
+		Main main = new Main();
+		Scanner sc = new Scanner(System.in);
+		int intEntrada;
+		
+		
+		login(main);
+		Empresa empresa = new Empresa(main.getNomeEmpresa());
+		
+		System.out.println("Seja bem vindo ao menu principal do Sistema de Controle de Eventos (**o menu visual será inserido posteriormente)");
+		
+		//LOOP Principal 
+		for(;;) {
+			mostrarMenuPrincipal(main);
+			
+			intEntrada = Utilitarios.lerInteiroComVerificacao();
+				
+			if(intEntrada == 1) {
+				System.out.println("Insira o nome do evento:");
+				String nome = sc.nextLine();
+				
+				System.out.println("Insira o tipo do evento:\n1-Festa\n2-Formatura\n3-Evento Corporativo");
+				Evento eventoAux;
+				int case1Entrada = sc.nextInt();
+				
+				
+				if(case1Entrada == 1) {
+					eventoAux = new Festa(nome);
+					empresa.cadastrarEvento(eventoAux);
+				}
+				
+				if(case1Entrada == 2) {
+					eventoAux = new Formatura(nome);
+					empresa.cadastrarEvento(eventoAux);
+				}
+				
+				if(case1Entrada == 3) {
+					eventoAux = new EventoCorporativo(nome);
+					empresa.cadastrarEvento(eventoAux);
+				}
+			}
+			
+			else if(intEntrada == 2) {
+				mostrarMenuEventos();
+				int case2Entrada = sc.nextInt();
+				sc.nextLine();
+				if(case2Entrada == 1) {
+					System.out.println("a");
+				}
+				if(case2Entrada == 2) {
+					System.out.println("Retornando ao menu principal");
+
+				}
+			}
+			
+			else if(intEntrada == 3) {
+				System.out.println("Em construção");
+			}	
+			
+			else if(intEntrada == 4) {
+				System.out.println("Em construção");
+			}	
+			
+			else if(intEntrada == 0) {
+				break;
+			}
+			
+			else {
+				System.out.println("O numero entrado não é uma das opções do menu!");
+			}
+				
+			
+
+		}
 	}
 }
---
+++ SistemaDeEventos/src/ucs/poo/trabalho_eventos/main/Utilitarios.java
@@ -0,0 +1,21 @@
+package ucs.poo.trabalho_eventos.main;
+
+import java.util.Scanner;
+
+public abstract class Utilitarios {
+		
+	//Criado essa função auxiliar, pois o sc.nextInt deixa o caractere de "Enter" (quebra de linha) no buffer.
+	//Alem disso ele ja faz a verificação de se a entrada é realmente um numero
+	public static int lerInteiroComVerificacao() {
+		Scanner sc = new Scanner(System.in);
+		String stringEntrada = sc.nextLine();
+		try {
+			int intEntrada = Integer.parseInt(stringEntrada);
+			return intEntrada;
+		}
+		catch(NumberFormatException e){
+			System.out.println("Entrada não é um numero");
+			return -1;
+		}
+	}
+}
