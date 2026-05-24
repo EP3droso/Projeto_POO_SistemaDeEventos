a4bea23b4578bfba31947ec50831004e0f349240
Eduardo <eduardo.hpedroso123@gmail.com>
2026-05-24 17:15:20 -0300

    Criação do menu base

--- SistemaDeEventos/src/ucs/poo/trabalho_eventos/main/Main.java
+++ SistemaDeEventos/src/ucs/poo/trabalho_eventos/main/Main.java
@@ -1,5 +1,34 @@
 package ucs.poo.trabalho_eventos.main;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String stringEntrada;
+		int intEntrada;
+		
+		System.out.println("Seja bem vindo ao menu principal do Sistema de Controle de Eventos, o menu visual será inserido posteriormente");
+		
+		while(!"0".equals(stringEntrada = sc.nextLine())) {
+			try {
+				intEntrada = Integer.parseInt(stringEntrada);
+				switch(intEntrada) {
+				case 1:
+					System.out.println("Em andamento");
+				}
+			}
+			catch(NumberFormatException e){
+				System.out.println("Entrada não corresponde a nenhuma das opções do menu");
+			}
+		}
+	}
+	
+	private void mostrarMenu() {
+		System.out.println("MENU");
+		System.out.println("1 - Cadastro de Evento");
+		System.out.println("2 - Menu de Eventos");
+		System.out.println("3 - Menu de Recursos");
+		System.out.println("4 - Relatorios");
+	}
 }
--- SistemaDeEventos/src/ucs/poo/trabalho_eventos/models/Empresa.java
+++ SistemaDeEventos/src/ucs/poo/trabalho_eventos/models/Empresa.java
@@ -1,7 +1,5 @@
 package ucs.poo.trabalho_eventos.models;
 
 public class Empresa {
-	public static void main(String[] args) {
-		System.out.println("teste2");
-	}
+
 }
